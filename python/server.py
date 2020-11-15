753322f5f032bc530c839c8ff06ae1eef7095020
Orlando <batres.93@gmail.com>
2020-11-14 18:42:05 -0600

    errores en  la insercion

--- python/server.py
+++ python/server.py
@@ -44,33 +44,38 @@ if convert(collection) in collist:
 def callback(ch, method, properties, body):
     
     print("recibido: %r" % body )
-    #mongodb
+    try:
+        #mongodb
 
-    conversion = convert(body)
-    print("CONVERSION " + str(conversion))
-    print("VARIABLE Y ")
-    y = json.loads(conversion)
-    print(y)
+        conversion = convert(body)
+        print("CONVERSION " + str(conversion))
+        print("VARIABLE Y ")
+        y = json.loads(conversion)
+        print(y)
 
-    for x in y["Casos"]:
-        mycol.insert_one(x)
+        for x in y["Casos"]:
+            mycol.insert_one(x)
 
-    #redis
-    r = redis.StrictRedis(host=IPREDIS, port=6379,db=0)
+        #redis
+        r = redis.StrictRedis(host=IPREDIS, port=6379,db=0)
 
-    pivote = convert(r.get(CONTADOR))
-    print("esto trae el contador    ", pivote)
-    for y in y["Casos"]:
-        r.hset(collection,NOMBRE+"["+pivote+"]", x[NOMBRE])
-        r.hset(collection,DEPARTAMENTO+"["+pivote+"]", x[DEPARTAMENTO])
-        r.hset(collection,EDAD+"["+pivote+"]", x[EDAD])
-        r.hset(collection,FORMA+"["+pivote+"]", x[FORMA])
-        r.hset(collection,ESTADO+"["+pivote+"]", x[ESTADO])
-        pivateInt = int(pivote) + 1
-        r.set(CONTADOR,pivateInt)
+        pivote = convert(r.get(CONTADOR))
+        print("esto trae el contador    ", pivote)
+        for y in y["Casos"]:
+            r.hset(collection,NOMBRE+"["+pivote+"]", x[NOMBRE])
+            r.hset(collection,DEPARTAMENTO+"["+pivote+"]", x[DEPARTAMENTO])
+            r.hset(collection,EDAD+"["+pivote+"]", x[EDAD])
+            r.hset(collection,FORMA+"["+pivote+"]", x[FORMA])
+            r.hset(collection,ESTADO+"["+pivote+"]", x[ESTADO])
+            pivateInt = int(pivote) + 1
+            r.set(CONTADOR,pivateInt)
 
-    print("Terminado")
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+        print("Terminado")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except:
+        err="Error en la insercion de datos   "
+        return(err)
+
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='proyecto2', on_message_callback= callback,auto_ack=False)
